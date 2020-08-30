60874112679c47a9774a659a6bbc7d313dd04903
qrdwqwq <3294300744@qq.com>
2020-08-31 00:26:50 +0800

    fisrt commit

---
+++ item/src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { age: 38, first_name: 'Jami', last_name: 'Carney' },
+          { age: 40, first_name: '志远', last_name: '张' },
+          { age: 24, first_name: '守约', last_name: '百里' },
+          { age: 22, first_name: '玄策', last_name: '百里' },
+          { age: 18, first_name: '婉儿', last_name: '上官' }
+        ]
+      }
+    }
+  }
+</script>
